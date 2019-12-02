6366711ac558dc62c16e6443e91aa1970c665f75
liulei548534 <549201079@qq.com>
2019-12-02 22:21:25 +0800

    12-2wan

--- miniprogram-2/app.js
+++ miniprogram-2/app.js
@@ -5,54 +5,54 @@ App({
      * 当小程序初始化完成时，会触发 onLaunch（全局只触发一次）
      */
     onLaunch: function () {
-        var that = this
-        wx.getLocation({
-            type: 'gcj02',
-            altitude: false,
-            success: (result) => {
-                const latitude1 = result.latitude
-                const longitude1 = result.longitude
-                console.log(longitude1 + "-----" + latitude1)
-                const latitude2 = 30.61859;
-                const longitude2 = 104.06776;
-                var EARTH_RADIUS = 6378.137; //地球半径
+        // var that = this
+        // wx.getLocation({
+        //     type: 'gcj02',
+        //     altitude: false,
+        //     success: (result) => {
+        //         const latitude1 = result.latitude
+        //         const longitude1 = result.longitude
+        //         console.log(longitude1 + "-----" + latitude1)
+        //         const latitude2 = 30.61859;
+        //         const longitude2 = 104.06776;
+        //         var EARTH_RADIUS = 6378.137; //地球半径
 
-                var num = getDistance(longitude1, latitude1, longitude2, latitude2)
+        //         var num = getDistance(longitude1, latitude1, longitude2, latitude2)
 
-                function rad(d) {
-                    return d * Math.PI / 180.0;
-                }
-                function getDistance(lng1, lat1, lng2, lat2) {
-                    var radLat1 = rad(lat1);
-                    var radLat2 = rad(lat2);
-                    var a = radLat1 - radLat2;
-                    var b = rad(lng1) - rad(lng2);
-                    var s = 2 * Math.asin(Math.sqrt(Math.pow(Math.sin(a / 2), 2)
-                        + Math.cos(radLat1) * Math.cos(radLat2)
-                        * Math.pow(Math.sin(b / 2), 2)));
-                    s = s * EARTH_RADIUS;
-                    s = Math.round(s * 10000) / 10000;
-                    return s;
-                }
-                console.log(num * 2000)
-                that.globalData.num=num*2000
-                // wx.openLocation({
-                //     latitude:latitude1,
-                //     longitude:longitude1,
-                //     scale: 18
-                //   })
-                // wx.chooseLocation({
-                //     latitude: latitude1,
-                //     longitude: longitude1,
-                //     scale: 18,
-                //     success: (result) => {
-                //         console.log(result)
-                //     }
-                // })
-            },
-            fail: () => { },
-            complete: () => { }
-        });
+        //         function rad(d) {
+        //             return d * Math.PI / 180.0;
+        //         }
+        //         function getDistance(lng1, lat1, lng2, lat2) {
+        //             var radLat1 = rad(lat1);
+        //             var radLat2 = rad(lat2);
+        //             var a = radLat1 - radLat2;
+        //             var b = rad(lng1) - rad(lng2);
+        //             var s = 2 * Math.asin(Math.sqrt(Math.pow(Math.sin(a / 2), 2)
+        //                 + Math.cos(radLat1) * Math.cos(radLat2)
+        //                 * Math.pow(Math.sin(b / 2), 2)));
+        //             s = s * EARTH_RADIUS;
+        //             s = Math.round(s * 10000) / 10000;
+        //             return s;
+        //         }
+        //         console.log(num * 2000)
+        //         that.globalData.num=num*2000
+        //         // wx.openLocation({
+        //         //     latitude:latitude1,
+        //         //     longitude:longitude1,
+        //         //     scale: 18
+        //         //   })
+        //         // wx.chooseLocation({
+        //         //     latitude: latitude1,
+        //         //     longitude: longitude1,
+        //         //     scale: 18,
+        //         //     success: (result) => {
+        //         //         console.log(result)
+        //         //     }
+        //         // })
+        //     },
+        //     fail: () => { },
+        //     complete: () => { }
+        // });
     },
 
     /**
--- miniprogram-2/pages/user/user.js
+++ miniprogram-2/pages/user/user.js
@@ -4,109 +4,173 @@ var status = true;
 var app = getApp();
 Page({
 
-    /**
-     * 页面的初始数据
-     */
-    data: {
-        show: false,
-        currenH: '',
-        currenTime: '',
-        status: status　　　　　　　　　　　　 //data里面的属性可以传递到视图
-    },
-    //触发toast
-    toastShow: function (e) {
-        if(app.globalData.num<500){
-            console.log("触发点击事件，弹出toast")
-            status = false
-        }else{
-            console.log("距离太远，打卡失败")
-            status = true
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    show: false,
+    currenH: '',
+    currenTime: '',
+    status: status,
+    　　　　　　　　　　　　 //data里面的属性可以传递到视图
+    info: "",
+    num: 0
+  },
+  //触发toast
+  toastShow: function(e) {
+    var that = this
+    var num = 0
+    wx.getLocation({
+      type: 'gcj02',
+      altitude: false,
+      success: (result) => {
+        const latitude1 = result.latitude
+        const longitude1 = result.longitude
+        console.log(longitude1 + "-----" + latitude1)
+        const latitude2 = 30.61859;
+        const longitude2 = 104.06776;
+        var EARTH_RADIUS = 6378.137; //地球半径
+
+        num = getDistance(longitude1, latitude1, longitude2, latitude2)
+
+        function rad(d) {
+          return d * Math.PI / 180.0;
         }
-        this.setData({ status: status })　　　　 //setData方法可以建立新的data属性，从而起到跟视图实时同步的效果
-    },
-    toastHide: function (e) {
-        console.log("触发bindchange，隐藏toast")
-        status = true
-        this.setData({ status: status })
-    },
 
-    /**
-     * 生命周期函数--监听页面加载
-     */
-    onLoad: function () {
-        var year = myDate.getFullYear()
-        var month = (myDate.getMonth() + 1)
-        var day = myDate.getDate()
-        if (month < 10) {
-            month = '0' + month
+        function getDistance(lng1, lat1, lng2, lat2) {
+          var radLat1 = rad(lat1);
+          var radLat2 = rad(lat2);
+          var a = radLat1 - radLat2;
+          var b = rad(lng1) - rad(lng2);
+          var s = 2 * Math.asin(Math.sqrt(Math.pow(Math.sin(a / 2), 2) +
+            Math.cos(radLat1) * Math.cos(radLat2) *
+            Math.pow(Math.sin(b / 2), 2)));
+          s = s * EARTH_RADIUS;
+          s = Math.round(s * 10000) / 10000;
+          return s;
         }
-        if (day < 10) {
-            day = '0' + day
-        }
-        var hours = myDate.getHours()
-        var minute = myDate.getMinutes()
-        if (hours < 10) {
-            hours = "0" + hours 
-        }
-        if (minute < 10) {
-            minute = '0' + minute
-        }
-        this.setData({
-            currenH: year + "/" + month + '/' + day,
-            currenTime: hours +":"+ minute,
-            show: true
-        })
-    },
-
-
-
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-
-    onUnload: function () {
-
-    },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh: function () {
-        console.log("刷新")
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom: function () {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage: function () {
+        console.log(num * 2000)
+        // that.data.num = num * 2000
+        num = num * 2000
+        // that.setData({
+        //   num: num * 2000
+        // })
+        // wx.openLocation({
+        //     latitude:latitude1,
+        //     longitude:longitude1,
+        //     scale: 18
+        //   })
+        // wx.chooseLocation({
+        //     latitude: latitude1,
+        //     longitude: longitude1,
+        //     scale: 18,
+        //     success: (result) => {
+        //         console.log(result)
+        //     }
+        // })
+      },
+      fail: () => {},
+      complete: () => {}
+    });
+    if (this.num < 500) {
+      // console.log("触发点击事件，弹出toast")
+      var info = "打卡成功"
+      status = false
+    } else {
+      info = "距离太远，打卡失败"
+      status = false
 
     }
+    this.setData({
+      status: status,
+      info
+    })　　　　 //setData方法可以建立新的data属性，从而起到跟视图实时同步的效果
+  },
+  toastHide: function(e) {
+    console.log("触发bindchange，隐藏toast")
+    status = true
+    this.setData({
+      status: status
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function() {
+    var year = myDate.getFullYear()
+    var month = (myDate.getMonth() + 1)
+    var day = myDate.getDate()
+    if (month < 10) {
+      month = '0' + month
+    }
+    if (day < 10) {
+      day = '0' + day
+    }
+    var hours = myDate.getHours()
+    var minute = myDate.getMinutes()
+    if (hours < 10) {
+      hours = "0" + hours
+    }
+    if (minute < 10) {
+      minute = '0' + minute
+    }
+    this.setData({
+      currenH: year + "/" + month + '/' + day,
+      currenTime: hours + ":" + minute,
+      show: true
+    })
+  },
+
+
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+
+  onUnload: function() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function() {
+    console.log("刷新")
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function() {
+
+  }
 })
